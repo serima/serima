4b11aea2dc719961e751b70c82775dca56f7e823
Ryo Shibayama <j02521@gmail.com>
2021-03-28 00:59:50 +0900

    Add update_recent_posts.php

---
+++ update_recent_posts.php
@@ -0,0 +1,58 @@
+<?php
+
+$posts = [];
+
+// Zenn
+$zenn =  simplexml_load_string(file_get_contents('https://zenn.dev/serima/feed'));
+foreach ($zenn->channel->item as $item) {
+    $posts[]  = [
+        'title' => $item->title,
+        'date' => date('Y-m-d H:i:s', strtotime($item->pubDate)),
+        'type' => 'zenn',
+        'url' => $item->link,
+    ];
+}
+
+// Qiita
+$qiita = simplexml_load_string(file_get_contents('https://qiita.com/serima/feed.atom'));
+foreach ($qiita->entry as $entry) {
+    $posts[]  = [
+        'title' => $entry->title,
+        'date' => date('Y-m-d H:i:s', strtotime($entry->published)),
+        'type' => 'qiita',
+        'url' => $entry->url,
+    ];
+}
+
+// note
+$note = simplexml_load_string(file_get_contents('https://note.com/serima/rss'));
+foreach ($note->channel->item as $item) {
+    $posts[]  = [
+        'title' => $item->title,
+        'date' => date('Y-m-d H:i:s', strtotime($item->pubDate)),
+        'type' => 'note',
+        'url' => $item->link,
+    ];
+}
+
+// Speaker Deck
+$speakerdeck = simplexml_load_string(file_get_contents('https://speakerdeck.com/serima.atom'));
+foreach ($speakerdeck->entry as $entry) {
+    $posts[]  = [
+        'title' => $entry->title,
+        'date' => date('Y-m-d H:i:s', strtotime($entry->published)),
+        'type' => 'speakerdeck',
+        'url' => $entry->link->attributes()->href,
+    ];
+}
+
+$sort_arr = array_map("strtotime", array_column($posts, "date"));
+array_multisort($sort_arr, SORT_DESC, $posts);
+
+$dist_md = '';
+
+foreach (array_splice($posts, 0, 7) as $post) {
+    $dist_md .= "- ![](img/${post['type']}.png) [${post['title']}](${post['url']})\n";
+}
+
+file_put_contents('README.md', preg_replace('/<!--\[START POSTS\]-->.*<!--\[END POSTS\]-->/s', "<!--[START POSTS]-->\n${dist_md}<!--[END POSTS]-->", file_get_contents('README.md')));
