336e67aeec7456b571cb30964a6a59f86a7020a4
Ryo Shibayama <j02521@gmail.com>
2021-03-28 01:08:14 +0900

    Fix image path

--- update_recent_posts.php
+++ update_recent_posts.php
@@ -52,7 +52,7 @@ array_multisort($sort_arr, SORT_DESC, $posts);
 $dist_md = '';
 
 foreach (array_splice($posts, 0, 7) as $post) {
-    $dist_md .= "- ![](img/${post['type']}.png) [${post['title']}](${post['url']})\n";
+    $dist_md .= "- ![](platform_icons/${post['type']}.png) [${post['title']}](${post['url']})\n";
 }
 
 file_put_contents('README.md', preg_replace('/<!--\[START POSTS\]-->.*<!--\[END POSTS\]-->/s', "<!--[START POSTS]-->\n${dist_md}<!--[END POSTS]-->", file_get_contents('README.md')));
